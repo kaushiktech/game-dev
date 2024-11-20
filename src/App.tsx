8eb9c96e1b7ee5490755a13fc35ead234b91d389
Kaushik Kundu <kaushik-tech@hotmail.com>
2024-11-19 22:00:31 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import {Button} from '@chakra-ui/react'
+import {Grid, GridItem} from '@chakra-ui/react'
+
 
 
 function App() {
-    return <Button colorPalette='blue'>Button</Button>
+    return <Grid templateAreas={{
+        base:`"nav" "main"`,
+        lg:`"nav nav" "aside main"`,
+    }}>
+        <GridItem hideFrom="" area='nav' bg='coral'>
+            Nav
+        </GridItem>
+
+
+        <GridItem hideBelow="lg"  area='aside' bg='gold'>
+            Aside
+        </GridItem>
+
+        <GridItem area='main' bg='dodgerblue'>
+            Main
+        </GridItem>
+    </Grid>
 }
 
 export default App
